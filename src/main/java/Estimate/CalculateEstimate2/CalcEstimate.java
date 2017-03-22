c89c8f1ccd540606811d7cc0f9b41b093d4d3f24
abhi <abhilasha.agrawal70@gmail.com>
2017-03-21 23:16:14 -0400

    adding basic methods for calculating estimate without checks

--- src/main/java/Estimate/CalculateEstimate2/CalcEstimate.java
+++ src/main/java/Estimate/CalculateEstimate2/CalcEstimate.java
@@ -1,5 +1,40 @@
-package Estimate.CalculateEstimate;
+package Estimate.CalculateEstimate2;
 
 public class CalcEstimate {
+	private static final double FLAT_MARKUP = 0.05;	
+	private static final double PERSON_MARKUP=0.012;
+	private static final double PHARM_MARKUP=0.075;
+	private static final double FOOD_MARKUP=0.13;
+	private static final double ELECTRONICS_MARKUP=0.02;
 
+		
+		public static double calFlatMarkup(double base_price)
+		{
+			return (base_price+(base_price*FLAT_MARKUP));
+		}
+		
+		public static double calculatePersonMarkup(double rate, int people)
+		{
+			return people*PERSON_MARKUP*rate;
+		}
+		
+		public static double calculateMarkups(double rate, String category)
+		{
+			
+			switch (category.toLowerCase())
+			{
+				case ("pharmaceuticals"):
+					 rate=rate*PHARM_MARKUP;
+					 break;
+				case "food":
+					 rate=rate*FOOD_MARKUP;
+					 break;	
+				case "electronics":
+					 rate=rate*ELECTRONICS_MARKUP;
+					 break;
+				default:
+					 return rate;
+			}
+				return rate;
+		}
 }
